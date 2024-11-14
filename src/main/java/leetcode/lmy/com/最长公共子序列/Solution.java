eb9c0898f06035cb34ff4c14e8c141aa1591ccf1
“lmy” <lmy1984cq@gmail.com>
2024-11-14 11:35:46 +0800

    最长公共子序列和编辑距离 本质都是根据末尾字符的比对，将问题又重新归结于子问题 另外对于降低成一维问题，其实就是将dp[i - 1][j - 1]问题，存储pre

--- src/main/java/leetcode/lmy/com/最长公共子序列/Solution.java
+++ src/main/java/leetcode/lmy/com/最长公共子序列/Solution.java
@@ -1,6 +1,12 @@
 package leetcode.lmy.com.最长公共子序列;
 
 public class Solution {
+    /**
+     * 其精髓在于原始问题划分为子问题时候的分类讨论
+     * @param text1
+     * @param text2
+     * @return
+     */
     public int longestCommonSubsequence(String text1, String text2) {
         int len1 = text1.length();
         int len2 = text2.length();
@@ -17,6 +23,29 @@ public class Solution {
         }
 
         return dp[len1][len2];
+    }
+
+    public int longestCommonSubsequence1(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+
+        int[] dp = new int[len2 + 1];
+
+        for (int i = 1; i <= len1; i ++) {
+            int pre = dp[0];
+            for (int j = 1; j <= len2; j ++) {
+                int tmp = dp[j];
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[j] = pre + 1;
+                } else {
+                    dp[j] = Math.max(dp[j - 1],dp[j]);
+                }
+
+                pre = tmp;
+            }
+        }
+
+        return dp[len2];
     }
 
     public static void main(String[] args) {
